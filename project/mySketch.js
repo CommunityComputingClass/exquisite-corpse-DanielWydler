0bc425481d2ee21326e082d154ab4bdd6e7f938b
DanielWydler <26daniel.wydler@lwhs.org>
2025-01-09 17:58:52 +0000

    submitting my first code

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,8 +1,20 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 500);
 }
 
 function draw() {
-  background(200);
   
+  background(235, 128, 52)
+  fill ("blue")
+  strokeWeight (2)
+  stroke ("white")
+  circle(150, 50, 40)
+
+  fill ("red")
+  strokeWeight (2)
+  stroke ("white")
+  circle (300, 50, 40)
+
+
+ 
 }
