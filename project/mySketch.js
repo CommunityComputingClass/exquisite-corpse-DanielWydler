f62613e4a9cf19c522a4289928926d785289055d
DanielWydler <26daniel.wydler@lwhs.org>
2025-01-13 23:32:30 +0000

    face

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,20 +1,33 @@
 function setup() {
   createCanvas(500, 500);
+
 }
 
 function draw() {
   
-  background(235, 128, 52)
+  background("pink")
+  
+   melo(200, 120)
+   melo(300, 50)
+   ball(250, 300)
+
+
+ 
+} 
+function melo (x,y){
   fill ("blue")
   strokeWeight (2)
   stroke ("white")
-  circle(150, 50, 40)
+  circle(x, y, 40)
 
   fill ("red")
   strokeWeight (2)
   stroke ("white")
-  circle (300, 50, 40)
+  circle (x+150, y, 40)
 
-
- 
+}
+function ball(x,y) {
+  fill("white")
+  stroke ("white")
+  circle (x, y, 40)
 }
