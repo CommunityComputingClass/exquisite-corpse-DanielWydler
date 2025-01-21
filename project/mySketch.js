5ded401f851499fe1ea82c07c1459b4a75462890
DanielWydler <26daniel.wydler@lwhs.org>
2025-01-21 20:02:47 +0000

    billy

--- project/mySketch.js
+++ project/mySketch.js
@@ -7,12 +7,10 @@ function draw() {
   
   background("pink")
   
-   melo(200, 120)
-   melo(300, 50)
-   ball(250, 300)
-
-
- 
+   melo(175, 120)
+   melo(175, 110)
+   ball(250, 160)
+   lavar(175,110)
 } 
 function melo (x,y){
   fill ("blue")
@@ -29,5 +27,11 @@ function melo (x,y){
 function ball(x,y) {
   fill("white")
   stroke ("white")
-  circle (x, y, 40)
+  circle (x, y, 60)
+}
+function lavar(x,y) {
+  fill ("black")
+  circle (x,y,20)
+  fill ("black")
+  circle (x+150,y,20)
 }
